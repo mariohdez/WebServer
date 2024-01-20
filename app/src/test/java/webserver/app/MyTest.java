f5b474443ef1998c47eeeacadac103c5b346e5e8
Mario Hernandez <mhmario963@gmail.com>
2024-01-20 17:30:12 -0500

    first commit

---
+++ app/src/test/java/webserver/app/MyTest.java
@@ -0,0 +1,11 @@
+package webserver.app;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MyTest {
+    @Test
+    public void dummyTest() {
+        Assertions.assertNotNull("hello");
+    }
+}
